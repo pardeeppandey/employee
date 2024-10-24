64a754d4944b01cb24a2dac92c664c63e23fae96
pardeeppandey <125869094+pardeeppandey@users.noreply.github.com>
2024-10-24 20:14:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+// Initialize express app
+const app = express();
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors());
+
+// MongoDB connection
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('MongoDB connected')).catch(err => console.log(err));
+
+// Routes for Admin and Employee API
+const adminRoutes = require('./routes/admin');
+const employeeRoutes = require('./routes/employee');
+const loginRoutes = require('./routes/authRoutes');
+
+app.use('/api/admin', adminRoutes);
+app.use('/api/employee', employeeRoutes);
+app.use('/api', loginRoutes);
+
+app.get('/',(req,res)=>{
+    res.status(400).send("Happy World")
+})
+
+// Start the server
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));

52d6401c144d592a01b4021cac0bf0c3ca82511e
pardeeppandey <125869094+pardeeppandey@users.noreply.github.com>
2024-10-24 19:55:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-require('dotenv').config();
-
-// Initialize express app
 const app = express();
+const port = 8000;
+
 app.use(express.json());
-app.use(bodyParser.json());
-app.use(cors());
 
-// MongoDB connection
-mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected')).catch(err => console.log(err));
+app.get('/', (req, res) => {
+    res.status(200).send('Hello Pardeep' );
+});
 
-// Routes for Admin and Employee API
 const adminRoutes = require('./routes/admin');
 const employeeRoutes = require('./routes/employee');
 const loginRoutes = require('./routes/authRoutes');
@@ -25,10 +16,7 @@ app.use('/api/admin', adminRoutes);
 app.use('/api/employee', employeeRoutes);
 app.use('/api', loginRoutes);
 
-app.get('/',(req,res)=>{
-    res.status(400).send("Happy World")
-})
 
-// Start the server
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});

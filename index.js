d961e1ec00980ab83fe2d3bff6ac0ccda5946dd0
pardeeppandey <125869094+pardeeppandey@users.noreply.github.com>
2024-10-24 19:56:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const adminRoutes = require('./routes/admin');
+const employeeRoutes = require('./routes/employee');
+const loginRoutes = require('./routes/authRoutes');
 const app = express();
 const port = 8000;
 
@@ -7,10 +10,6 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.status(200).send('Hello Pardeep' );
 });
-
-const adminRoutes = require('./routes/admin');
-const employeeRoutes = require('./routes/employee');
-const loginRoutes = require('./routes/authRoutes');
 
 app.use('/api/admin', adminRoutes);
 app.use('/api/employee', employeeRoutes);

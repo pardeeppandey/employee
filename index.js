45f47a1658e754517a472dd44ee17a552ea0d3e2
pardeeppandey <125869094+pardeeppandey@users.noreply.github.com>
2024-10-24 11:41:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,3 +31,5 @@ app.use('/api', loginRoutes);
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
